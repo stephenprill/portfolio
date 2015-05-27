338e17202cdd7a36a6c4d23a753bd0ab9d59fc45
Stephen Prill <stephen.prill@gmail.com>
2015-05-26 21:30:54 -0600

    before moving things around

--- app/router.js
+++ app/router.js
@@ -7,9 +7,11 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('projects', function() {
+    // this.resource('project', { 'path' : '/:project_id' });
     this.resource('project', { path:'/:project_id' });
   });
   this.route('resume');
 });
+
 
 export default Router;

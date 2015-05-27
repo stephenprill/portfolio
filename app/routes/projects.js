c5218e8f5b8e29db63db4306eb9d120f0110fd75
Stephen Prill <stephen.prill@gmail.com>
2015-05-26 18:02:21 -0600

    added localnotes and spoti and pictures of each and roughly formatted

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -4,18 +4,31 @@ export default Ember.Route.extend({
   model: function () {
     return [{
       id: 1,
-      name: "gCamp",
+      name: "Spotibility",
       description: "It's like Basecamp but better.",
+      images: 'pictures/spotibility.png',
+      url: 'https://spotibility.herokuapp.com',
     },
     {
       id: 2,
-      name: "Listen",
-      description: "You can listen to the classic jams of your past.",
+      name: "Local Notes",
+      description: "Best place to take notes without a internet connection",
+      images: 'pictures/localnotes.png',
+      url: 'https://localnotes.herokuapp.com/notes',
     },
     {
       id: 3,
+      name: "gCamp",
+      description: "It's like Basecamp but better.",
+      images: 'pictures/gcamp.png',
+      url: 'https://gcamp-prill.herokuapp.com',
+    },
+    {
+      id: 4,
       name: "Rantly",
       description: "This is like Twitter.",
+      images: 'pictures/rantly.png',
+      url: 'https://rantly-prill.herokuapp.com',
     }];
   }
 });

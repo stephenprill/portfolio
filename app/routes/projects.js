54090763afd27b8ed7efeccd56fbf55c7481f95a
Stephen Prill <stephen.prill@gmail.com>
2015-06-03 11:10:12 -0600

    added css for projects.hbs, added new resume, synced with domain, hover works

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -5,30 +5,34 @@ export default Ember.Route.extend({
     return [{
       id: 1,
       name: "Spotibility",
-      description: "A Ruby on Rails app that allows users to compare their musical tastes via a calculated match percentage through the Spotify API.",
+      description: "Spotibility is a Ruby on Rails app that allows users to compare their musical tastes via a calculated match percentage through the Spotify API.",
       images: 'pictures/spotibility_small.png',
       url: 'https://spotibility.herokuapp.com',
+      github: 'https://github.com/stephenprill/spotibility'
     },
     {
       id: 2,
       name: "gCamp",
-      description: "A Basecamp clone built in Ruby on Rails with Twitter Bootstrap, postgreSQL, and Pivotal Tracker API.",
+      description: "gCamp is a Basecamp clone built in Ruby on Rails with Twitter Bootstrap, postgreSQL, and Pivotal Tracker API.",
       images: 'pictures/gcamp_small.png',
       url: 'https://gcamp-prill.herokuapp.com',
+      github: 'https://github.com/stephenprill/gCamp'
     },
     {
       id: 3,
       name: "Local Notes",
-      description: "Ember CLI frontend and Rails API backend and allows you to store local notes on your computer.",
+      description: "Local Notes has an Ember CLI frontend and Rails API backend and allows user to store local notes on your computer.",
       images: 'pictures/localnotes_small.png',
       url: 'https://localnotes.herokuapp.com/notes',
+      github: 'https://github.com/stephenprill/ember-notes'
     },
     {
       id: 4,
       name: "Rantly",
-      description: "This is like Twitter but you have to have over 140 characters in order to rant.",
+      description: "Rantly is a Twitter clone built in Rails where user requires over 140 characters in order to post a rant.",
       images: 'pictures/rantly_small.png',
       url: 'https://rantly-prill.herokuapp.com',
+      github: 'https://github.com/stephenprill/rantly'
     }];
   }
 });

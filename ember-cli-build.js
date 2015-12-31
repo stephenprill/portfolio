c03c9e5bf16b26ccd807a0b9d02ddeadc27b7fec
Stephen Prill <stephen.prill@gmail.com>
2015-12-30 22:07:35 -0700

    removed brocfile.js for ember-cli-build.js and updated ember

---
+++ ember-cli-build.js
@@ -0,0 +1,12 @@
+var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+
+module.exports = function(defaults) {
+    var app = new EmberApp(defaults, {
+        // Any other options
+    });
+
+    // app.import("file1.js");
+    // app.import("file2.js");
+
+    return app.toTree();
+};
